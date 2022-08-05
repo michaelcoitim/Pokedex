ee83472befa029b2d12539ae3535bf8215103b3d
michaelcoitim <michaelcoitim1@gmail.com>
2022-08-04 22:09:34 -0300

    Função Consumido api

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-const fetchPokemon = (pokemon) =>{
+const fetchPokemon = async (pokemon) =>{
 
-    const APIResponse= fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    console.log(APIResponse);
+    const APIResponse= await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    // variavel pega o json da api 
+    const dados =await APIResponse.json();
+    // retona o json com as informações do pokemon 
+    return data;
+
+    //prita dados do objeto no console
+    //console.log(dados);
 }
-
-fetchPokemon();
+// teste chamado função consumindo api
+fetchPokemon('25');
