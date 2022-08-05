d2783c88f5894d7eb57aa4bfb658a65b45696536
michaelcoitim <michaelcoitim1@gmail.com>
2022-08-05 10:48:28 -0300

    erro nome

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
-const fetchPokemon = async (pokemon) =>{
+const nomePokemon = document.querySelector('.pokemon_name');
+const numeroPokemon = document.querySelector('.pokemon_number');
 
-    const APIResponse= await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
-    // variavel pega o json da api 
-    const dados =await APIResponse.json();
-    // retona o json com as informações do pokemon 
-    return data;
+// função pega dados da api e tranforma json
+const fetchPokemon = async (pokemon) => { 
+        const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+        // variavel pega o json da api 
+        const dados = await APIResponse.json();
+        // retona o json com as informações do pokemon 
+        return dados;
 
     //prita dados do objeto no console
-    //console.log(dados);
+  //  console.log(dados);
 }
 // teste chamado função consumindo api
-fetchPokemon('25');
+//fetchPokemon('25');
+
+
+
+// função renderiza (desenha o pokemon)
+const renderPokemon = async (pokemon) => {
+    // chamado ''json''
+    const dados = await fetchPokemon(pokemon);
+
+    //setando nome do pokemon no html
+        nomePokemon.innerHTML = dados.name;
+}
+// teste renderizando
+renderPokemon('25');
